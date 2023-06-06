2c989a9fa325a68c32bd1454189e10175923a025
0xdiego <93290842+diegojoel301@users.noreply.github.com>
2023-06-06 17:32:09 -0400

    Add files via upload

---
+++ Tercer_Ejercicio/conexion/Form1.Designer.cs
@@ -0,0 +1,362 @@
+﻿namespace conexion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnColor = new System.Windows.Forms.Button();
+            this.btnColorear = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.txtB = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtG = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtR = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnAbrir = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnColor
+            // 
+            this.btnColor.BackColor = System.Drawing.Color.Silver;
+            this.btnColor.FlatAppearance.BorderSize = 0;
+            this.btnColor.ForeColor = System.Drawing.Color.Black;
+            this.btnColor.Location = new System.Drawing.Point(804, 598);
+            this.btnColor.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnColor.Name = "btnColor";
+            this.btnColor.Size = new System.Drawing.Size(224, 73);
+            this.btnColor.TabIndex = 54;
+            this.btnColor.Text = "Establecer Color";
+            this.btnColor.UseVisualStyleBackColor = false;
+            this.btnColor.Click += new System.EventHandler(this.btnColor_Click);
+            // 
+            // btnColorear
+            // 
+            this.btnColorear.BackColor = System.Drawing.Color.Silver;
+            this.btnColorear.FlatAppearance.BorderSize = 0;
+            this.btnColorear.ForeColor = System.Drawing.Color.Black;
+            this.btnColorear.Location = new System.Drawing.Point(804, 733);
+            this.btnColorear.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnColorear.Name = "btnColorear";
+            this.btnColorear.Size = new System.Drawing.Size(224, 46);
+            this.btnColorear.TabIndex = 52;
+            this.btnColorear.Text = "Colorear";
+            this.btnColorear.UseVisualStyleBackColor = false;
+            this.btnColorear.Click += new System.EventHandler(this.btnColorear_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(1088, 279);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1484, 273);
+            this.dataGridView1.TabIndex = 50;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.Control;
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(802, 279);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(196, 25);
+            this.label4.TabIndex = 49;
+            this.label4.Text = "Nombre de Textura";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(834, 325);
+            this.txtNombre.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(222, 31);
+            this.txtNombre.TabIndex = 48;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.BackColor = System.Drawing.Color.Silver;
+            this.btnGuardar.FlatAppearance.BorderSize = 0;
+            this.btnGuardar.ForeColor = System.Drawing.Color.Black;
+            this.btnGuardar.Location = new System.Drawing.Point(804, 679);
+            this.btnGuardar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(224, 46);
+            this.btnGuardar.TabIndex = 47;
+            this.btnGuardar.Text = "Guardar BD";
+            this.btnGuardar.UseVisualStyleBackColor = false;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // txtB
+            // 
+            this.txtB.Location = new System.Drawing.Point(874, 219);
+            this.txtB.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(148, 31);
+            this.txtB.TabIndex = 45;
+            this.txtB.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.Control;
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(796, 219);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 25);
+            this.label3.TabIndex = 44;
+            this.label3.Text = "B:";
+            // 
+            // txtG
+            // 
+            this.txtG.Location = new System.Drawing.Point(874, 165);
+            this.txtG.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtG.Name = "txtG";
+            this.txtG.Size = new System.Drawing.Size(148, 31);
+            this.txtG.TabIndex = 43;
+            this.txtG.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.Control;
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(796, 165);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 25);
+            this.label2.TabIndex = 42;
+            this.label2.Text = "G:";
+            // 
+            // txtR
+            // 
+            this.txtR.Location = new System.Drawing.Point(874, 115);
+            this.txtR.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtR.Name = "txtR";
+            this.txtR.Size = new System.Drawing.Size(148, 31);
+            this.txtR.TabIndex = 41;
+            this.txtR.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.Control;
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(798, 115);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 25);
+            this.label1.TabIndex = 40;
+            this.label1.Text = "R:";
+            // 
+            // btnAbrir
+            // 
+            this.btnAbrir.BackColor = System.Drawing.Color.Silver;
+            this.btnAbrir.FlatAppearance.BorderSize = 0;
+            this.btnAbrir.ForeColor = System.Drawing.Color.Black;
+            this.btnAbrir.Location = new System.Drawing.Point(808, 38);
+            this.btnAbrir.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnAbrir.Name = "btnAbrir";
+            this.btnAbrir.Size = new System.Drawing.Size(224, 50);
+            this.btnAbrir.TabIndex = 39;
+            this.btnAbrir.Text = "Abrir Imagen";
+            this.btnAbrir.UseVisualStyleBackColor = false;
+            this.btnAbrir.Click += new System.EventHandler(this.btnAbrir_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(26, 13);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(762, 1085);
+            this.pictureBox1.TabIndex = 38;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.SystemColors.Control;
+            this.label5.ForeColor = System.Drawing.Color.Black;
+            this.label5.Location = new System.Drawing.Point(803, 387);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(265, 25);
+            this.label5.TabIndex = 55;
+            this.label5.Text = "Valores para Nuevo Color:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(876, 537);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(148, 31);
+            this.textBox1.TabIndex = 61;
+            this.textBox1.Text = "0";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.SystemColors.Control;
+            this.label6.ForeColor = System.Drawing.Color.Black;
+            this.label6.Location = new System.Drawing.Point(798, 537);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(32, 25);
+            this.label6.TabIndex = 60;
+            this.label6.Text = "B:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(876, 483);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(148, 31);
+            this.textBox2.TabIndex = 59;
+            this.textBox2.Text = "0";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.SystemColors.Control;
+            this.label7.ForeColor = System.Drawing.Color.Black;
+            this.label7.Location = new System.Drawing.Point(798, 483);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(34, 25);
+            this.label7.TabIndex = 58;
+            this.label7.Text = "G:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(876, 433);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(148, 31);
+            this.textBox3.TabIndex = 57;
+            this.textBox3.Text = "0";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.SystemColors.Control;
+            this.label8.ForeColor = System.Drawing.Color.Black;
+            this.label8.Location = new System.Drawing.Point(800, 433);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(33, 25);
+            this.label8.TabIndex = 56;
+            this.label8.Text = "R:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(2324, 1148);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnColor);
+            this.Controls.Add(this.btnColorear);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.txtB);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtG);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtR);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnAbrir);
+            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnColor;
+        private System.Windows.Forms.Button btnColorear;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.TextBox txtB;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtG;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtR;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnAbrir;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label8;
+
+
+    }
+}
+
